dffa8dd0c4fad83bd2b40e1a32d6fda9c45a9358
Undisclosed <undisclosed674@gmail.com>
2021-08-21 06:48:23 -0700

    add test for reverseString function

--- script.test.js
+++ script.test.js
@@ -5,3 +5,20 @@ test('capitalizes the first letter',()=>{
 function capitalize(string){
     return string.charAt(0).toUpperCase() + string.slice(1);
 };
+
+//test2
+test('reverses a string',() =>{
+   expect(reverseString('Hello')).toMatch('olleH');
+});
+
+function reverseString(str){
+// splits the string into an arr
+const splitString = str.split("");
+// reverse() method to reverse the newly created array
+const reverseArr = splitString.reverse();
+//use join method to join the arr
+const joinArr = reverseArr.join("");
+//return str.split("").reverse().join(""),can also be done in one line
+return joinArr;
+
+}
