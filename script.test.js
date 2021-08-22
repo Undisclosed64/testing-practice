22330f35876190d8aa137467c0929a91e708b40e
Undisclosed <undisclosed674@gmail.com>
2021-08-22 01:15:16 -0700

    add test for the find function

--- script.test.js
+++ script.test.js
@@ -26,10 +26,45 @@ function reverseString(str){
 //test3
 test('checks the object for the specified key and value',() =>{
   const calc = {
-    add:3 + 2,
+    add:2 + 2,
     subtract:5-4,
     divide:10/5,
     multiply:10*5
   }
-expect(calc).toStrictEqual({add:3 + 2,subtract:5-4, divide:10/5,multiply:10*5})
+expect(calc).toStrictEqual({add:2+2,subtract:5-4, divide:10/5,multiply:10*5})
 });
+
+//test4
+test("Checks if the obj has the specific properties",() => {
+  expect(numArr).toHaveProperty('average');
+  expect(numArr).toHaveProperty('min');
+ expect(numArr).toHaveProperty('max');
+  expect(numArr).toHaveProperty('length');
+})
+
+let numArr = [3,6,8,9,2,12];
+function calc(){
+
+  function average(){
+    let total = 0;
+    for(let i = 0;i<numArr.length;i++){
+      total = numArr[i] + total;
+    }
+    const avg = Math.floor(total/numArr.length);
+    return avg;
+  }
+  function min(){
+    return Math.min.apply(null, numArr);
+  }
+  function max(){
+    return Math.max.apply(null, numArr);
+  }
+  
+  return numArr = {
+   average:average(),
+   min:min(),
+   max:max(),
+   length:numArr.length,
+  }
+}
+console.log(calc());
